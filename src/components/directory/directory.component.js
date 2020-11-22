7dac316c9f8812c47009ba053967109a773e979b
David Acosta <acosta816@gmail.com>
2020-11-21 20:01:28 -0800

    created menu-item, directory, homepage components as well as their stylings. Next: add routing to respective homepage link routes

---
+++ src/components/directory/directory.component.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import MenuItem from '../menu-item/menu-item.component';
+import './directory.styles.scss';
+import sectionsData from './sections-data';
+
+export default class Directory extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sections: sectionsData
+        }
+    };
+
+    render() {
+        return (
+            <div className="directory-menu">
+                {
+                    this.state.sections.map(({ title, id, imageUrl, size }) => (
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    ))
+                }
+            </div>
+
+        )
+    }
+
+}
